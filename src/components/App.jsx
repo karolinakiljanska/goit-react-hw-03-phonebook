4f6ef4622c4c6afe10cfb45ebfdbf0cd90704b25
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-09-16 17:56:23 +0200

    dodanie cykli życia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,20 @@ class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
 
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contact) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
 
